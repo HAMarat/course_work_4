81bc68d1f537ff788f15f98d04ac65de60547a32
Huawei <smile56680@gmail.com>
2023-02-20 19:31:08 +0300

    feat: add models, create tables and add data to table

--- project/config.py
+++ project/config.py
@@ -33,7 +33,8 @@ class TestingConfig(BaseConfig):
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR.joinpath('project.db').as_posix()
+    SQLALCHEMY_DATABASE_URI = "sqlite:///movies.db"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR.joinpath('project.db').as_posix()
 
 
 class ProductionConfig(BaseConfig):
@@ -46,13 +47,13 @@ class ConfigFactory:
 
     @classmethod
     def get_config(cls) -> Type[BaseConfig]:
-        if cls.flask_env == 'development':
-            return DevelopmentConfig
-        elif cls.flask_env == 'production':
-            return ProductionConfig
-        elif cls.flask_env == 'testing':
-            return TestingConfig
-        raise NotImplementedError
+        # if cls.flask_env == 'development':
+        return DevelopmentConfig
+        # elif cls.flask_env == 'production':
+        #     return ProductionConfig
+        # elif cls.flask_env == 'testing':
+        # TestingConfig
+        # raise NotImplementedError
 
 
 config = ConfigFactory.get_config()
--- project/models.py
+++ project/models.py
@@ -1,9 +1,40 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 from project.setup.db import models
 
 
 class Genre(models.Base):
-    __tablename__ = 'genres'
+    __tablename__ = 'genre'
 
     name = Column(String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = 'director'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = 'movie'
+
+    title = Column(String(100))
+    description = Column(String(250))
+    trailer = Column(String(200))
+    year = Column(Integer)
+    rating = Column(Float(100))
+    genre_id = Column(Integer, ForeignKey("genre.id"))
+    genre = relationship("Genre")
+    director_id = Column(Integer, ForeignKey("director.id"))
+    director = relationship("Director")
+
+
+class User(models.Base):
+    __tablename__ = 'user'
+
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    surname = Column(String(100))
+    favorite_genre = Column(String(100))
